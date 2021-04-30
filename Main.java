8e0ce3ea951d1fb5c8a4c175f4c352fb49afa729
Idrissa Muwanga <80053968+IdrissaMuwanga@users.noreply.github.com>
2021-04-30 16:23:49 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.awt.Point;
+
+public class Main {
+	Point A= new Point(2,5);
+	Point B= new Point(8,8);
+	Point C= new Point(10,1);
+	Triangle tri= new Triangle(A, B, C);
+	
+
+}
---
+++ Triangle.java
@@ -0,0 +1,22 @@
+import java.awt.Point;
+
+public class Triangle {
+	
+	
+	private Point Pa, Pb, Pc;
+	public Triangle(Point A, Point B, Point C) {
+		Pa=A;
+		Pb=B;
+		Pc=C;
+		
+	}
+public static void affiche() {
+	System.out.println("TriangleABC :");
+	System.out.println("A: ");Triangle.affiche();
+	System.out.println("B: ");Triangle.affiche();
+	System.out.println("C: ");Triangle.affiche();
+}
+
+}
+
+
